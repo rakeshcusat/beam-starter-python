3561c07a3b6cae70ea9e832032a318c58b4081c0
Rakesh Kumar <rakeshcusat@gmail.com>
2025-05-04 00:15:16 -0700

    working stateful processing with timestamp

--- stateful_app/app.py
+++ stateful_app/app.py
@@ -10,40 +10,50 @@ from typing import Callable
 from typing import Optional
 from collections.abc import Generator
 
+import time
+
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms import userstate
 
 
-BASE_TIMESTAMP = 1746339915
+BASE_TIMESTAMP = int(time.time())
 ONE_MINUTE = 60
-NAME_FIELD = 'name'
-TIMESTAMP_FIELD = 'timestamp'
+
+TIMESTAMP_FIELD = 'TIMESTAMP'
+KEY_FIELD = 'KEY'
+VALUE_FIELD = 'VALUE'
 
 INPUT_DATA = [
-    {'name': 'data1', 'timestamp': BASE_TIMESTAMP},
-    {'name': 'data2', 'timestamp': BASE_TIMESTAMP},
-    {'name': 'data3', 'timestamp': BASE_TIMESTAMP},
-    {'name': 'data4', 'timestamp': BASE_TIMESTAMP},
-    {'name': 'data5', 'timestamp': BASE_TIMESTAMP},
-    {'name': 'data6', 'timestamp': BASE_TIMESTAMP + 1 * ONE_MINUTE},
-    {'name': 'data7', 'timestamp': BASE_TIMESTAMP + 1 * ONE_MINUTE},
-    {'name': 'data8', 'timestamp': BASE_TIMESTAMP + 1 * ONE_MINUTE},
-    {'name': 'data9', 'timestamp': BASE_TIMESTAMP + 1 * ONE_MINUTE},
-    {'name': 'data10', 'timestamp': BASE_TIMESTAMP + 1 * ONE_MINUTE},
-    {'name': 'data11', 'timestamp': BASE_TIMESTAMP + 2 * ONE_MINUTE},
-    {'name': 'data12', 'timestamp': BASE_TIMESTAMP + 2 * ONE_MINUTE},
-    {'name': 'data13', 'timestamp': BASE_TIMESTAMP + 2 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data1', TIMESTAMP_FIELD: BASE_TIMESTAMP},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data2', TIMESTAMP_FIELD: BASE_TIMESTAMP},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data3', TIMESTAMP_FIELD: BASE_TIMESTAMP},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data4', TIMESTAMP_FIELD: BASE_TIMESTAMP},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data5', TIMESTAMP_FIELD: BASE_TIMESTAMP},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data6', TIMESTAMP_FIELD: BASE_TIMESTAMP + 1 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data7', TIMESTAMP_FIELD: BASE_TIMESTAMP + 1 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data8', TIMESTAMP_FIELD: BASE_TIMESTAMP + 1 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data9', TIMESTAMP_FIELD: BASE_TIMESTAMP + 1 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data10', TIMESTAMP_FIELD: BASE_TIMESTAMP + 1 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data11', TIMESTAMP_FIELD: BASE_TIMESTAMP + 2 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data12', TIMESTAMP_FIELD: BASE_TIMESTAMP + 2 * ONE_MINUTE},
+    {KEY_FIELD: 'key1', VALUE_FIELD: 'data13', TIMESTAMP_FIELD: BASE_TIMESTAMP + 2 * ONE_MINUTE},
 ]
 
 
-def split_words(input_string_list: str) ->  list[str]:
-    result = []
-    print(f"input_string_list type: {type(input_string_list)}")
+class IndexAssigningStatefulDoFn(beam.DoFn):
+  """
+  This is a stateful processing class which is providing an index to each incoming message.
+  """
+  INDEX_STATE = userstate.CombiningValueStateSpec(name='index', coder=sum)
 
-    for input_string in input_string_list:
-        for item in input_string.split(','):
-            result.append(item)
-    return result
+  def process(self, element, index=beam.DoFn.StateParam(INDEX_STATE)):
+    unused_key, value = element
+    current_index = index.read()
+    index.add(1)
+    yield (value, current_index)
+
+
 
 def run(
     input_text: str,
@@ -55,8 +65,10 @@ def run(
             pipeline
             | "Create elements" >> beam.Create(INPUT_DATA)
             | 'With timestamps' >> beam.Map(
-            lambda item: beam.window.TimestampedValue(item[NAME_FIELD], 
-                                                      item[TIMESTAMP_FIELD]))
+            lambda item: beam.window.TimestampedValue(value=item, 
+                                                      timestamp=item[TIMESTAMP_FIELD]))
+            | "Set key and value" >> beam.Map(lambda item: (item[KEY_FIELD], item[VALUE_FIELD]))
+            | "Stateful processing" >> beam.ParDo(IndexAssigningStatefulDoFn())                                          
             | "Print elements" >> beam.Map(print)
         )
 
